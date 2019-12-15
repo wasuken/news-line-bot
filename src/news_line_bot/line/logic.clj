e2e8e62b01627cf8ac25828df12c441ed8b1d4b6
wasuken <wevorence@gmail.com>
2019-12-15 23:50:37 +0900

    fix

--- src/news_line_bot/line/logic.clj
+++ src/news_line_bot/line/logic.clj
@@ -4,7 +4,8 @@
             [ring.util.response :as response]
             [clojure.tools.logging :as log])
   (:import [com.linecorp.bot.client LineMessagingClient]
-            [com.linecorp.bot.model ReplyMessage]))
+           [com.linecorp.bot.model ReplyMessage]
+           [com.linecorp.bot.model.message TextMessage]))
 
 (def line-bot-client
   (.build (LineMessagingClient/builder (env :line-channel-token))))
@@ -17,7 +18,7 @@
     (cond (= (get event-json "type") "message")
           (let [message-text (get (get event-json "message") "text")]
             (log/debug "=======================")
-            (let [resp (.get (.replyMessage line-bot-client (ReplyMessage. reply-token "test")))]
+            (let [resp (.get (.replyMessage line-bot-client (ReplyMessage. reply-token (TextMessage. "test"))))]
               (log/debug (.getRequestId resp))
               (log/debug (.getMessage resp))
               (log/debug (.getDetails resp))
