7e77f5623bc129a72719bde81a9faab779432a30
wasuken <wevorence@gmail.com>
2019-12-15 14:58:24 +0900

    project.clj

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [environ "1.1.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]]
+  :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler news-line-bot.core/app}
   :target-path "target/%s"
